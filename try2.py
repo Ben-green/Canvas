6cafbce200ce98a57492c890d2a469d2a841586b
Ben Fowler <ben.greencanvas@gmail.com>
2015-03-25 12:37:13 +0000

    Use try2.py to test the tent module

--- try2.py
+++ try2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # vim: tabstop=4 softtabstop=4 shiftwidth=4 smarttab expandtab:
 
-"""try2.py: Tests the tent2 module"""
+"""try2.py: Tests the tent module"""
 
 __author__      = "Joe Salmon"
 __copyright__   = "Copyright 2015, Planet Earth"
 
-import tent2
+import tent
 import sqlite3
 
 filenamedb = 'something.db'
@@ -17,4 +17,4 @@ cur = conn.cursor()
 
 
 thingy = 'work/councilfull.csv'
-tent2.importcsvfile (thingy,filenamedb)
+tent.importcsvfile (thingy,filenamedb)
